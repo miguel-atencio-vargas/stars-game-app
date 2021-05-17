892d1dd4d9c5577deef660aaa29e51647d12cb2e
skyfall947 <atencio947@gmail.com>
2021-05-17 18:08:11 -0400

    introducing useEffect and little changes added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import {colors, math} from './utils/game.utils';
+import { colors, math } from './utils/game.utils';
 
 const StarsDisplay = props => {
   return (
@@ -15,39 +15,56 @@ const StarsDisplay = props => {
 };
 
 const PlayNumber = props => (
-  <button 
+  <button
     className="number"
-    style={{backgroundColor: colors[props.status]}}
+    style={{ backgroundColor: colors[props.status] }}
     onClick={() => props.onClick(props.number, props.status)}>
-      {props.number}
+    {props.number}
   </button>
 )
 
 const PlayAgain = props => (
   <div className='game-done'>
+    <div 
+      className='message'
+      style={{color: props.gameStatus === 'lost' ? 'red' : 'green'}}
+    >
+      {props.gameStatus === 'lost' ? 'Game Over' : 'Nice you win!'}
+    </div>
     <button onClick={props.onClick}>Play Again</button>
   </div>
 )
 
 const StarMatch = () => {
-  const [stars, setStars] = useState(math.random(1,9));
-  const [availableNums,setAvailableNums] = useState(math.range(1, 9));
+  const [stars, setStars] = useState(math.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(math.range(1, 9));
   const [candidateNums, setCandidateNums] = useState([]);
+  const [secondsLeft, setSecondsLeft] = useState(10);
+
+  useEffect(() => {
+    if (secondsLeft > 0 && availableNums.length > 0) {
+      const timerID = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, 1000);
+      return () => clearTimeout(timerID);
+    }
+  });
 
   const isCandidatesWrong = math.sum(candidateNums) > stars;
-  const isGameDone = availableNums.length === 0;
+  const gameStatus = availableNums.length === 0
+    ? 'won'
+    : secondsLeft === 0 ? 'lost' : 'active';
 
   const resetGame = () => {
-    // this is enough when there is not side efects
     setStars(math.random(1, 9));
     setAvailableNums(math.range(1, 9));
     setCandidateNums([]);
   }
 
   const numberStatus = number => {
-    if(!availableNums.includes(number)) return 'used';
+    if (!availableNums.includes(number)) return 'used';
 
-    if(candidateNums.includes(number)) {
+    if (candidateNums.includes(number)) {
       return isCandidatesWrong ? 'wrong' : 'candidate'
     }
 
@@ -55,13 +72,13 @@ const StarMatch = () => {
   }
 
   const onNumberClick = (number, currentStatus) => {
-    if(currentStatus === 'used') return;
-    
+    if (currentStatus === 'used' || gameStatus !== 'active') return;
+
     const newCandidateNums =
       currentStatus === 'available'
         ? candidateNums.concat(number)
         : candidateNums.filter(cNum => cNum !== number);
-    if(math.sum(newCandidateNums) !== stars) {
+    if (math.sum(newCandidateNums) !== stars) {
       setCandidateNums(newCandidateNums)
     } else {
       const newAvailabeNums = availableNums.filter(
@@ -81,17 +98,17 @@ const StarMatch = () => {
       <div className="body">
 
         <div className="left">
-          {isGameDone ? (
-            <PlayAgain onClick={resetGame} />
+          {gameStatus !== 'active' ? (
+            <PlayAgain onClick={resetGame} gameStatus={gameStatus} />
           ) : (
-            <StarsDisplay count = { stars } />
-          ) 
+            <StarsDisplay count={stars} />
+          )
           }
         </div>
 
         <div className="right">
-          {math.range(1, 9).map(number => 
-            <PlayNumber 
+          {math.range(1, 9).map(number =>
+            <PlayNumber
               key={number}
               number={number}
               status={numberStatus(number)}
@@ -101,7 +118,7 @@ const StarMatch = () => {
         </div>
 
       </div>
-      <div className="timer">Time Remaining: 10</div>
+      <div className="timer">Time Remaining: {secondsLeft}</div>
     </div>
   );
 };
